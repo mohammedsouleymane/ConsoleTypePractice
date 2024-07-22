7330baa73d5b3a5f444b68f51821cdd0fa281363
GeeGee <mohammedsouleymane@outlook.com>
2024-07-22 17:48:16 +0200

    fix restart and exit

--- ConsoleTypePractice/Program.cs
+++ ConsoleTypePractice/Program.cs
@@ -1,39 +1,41 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using ConsoleTypePractice;
 
-
-using ConsoleTypePractice;
- 
-
- void Start()
+while (true)
 {
     var center = (Console.WindowWidth - 80) / 2;
-    var height = Console.WindowHeight / 3;
+    var height = Console.WindowHeight / 4;
     Console.Clear();
     Console.ResetColor();
     var randomWords = RandomWordGen.RandomWords();
     var typed = "";
     var currentLine = 0;
-    
-   
+
+
     foreach (var line in randomWords)
     {
-        Console.SetCursorPosition(center, height + currentLine++ );
+        Console.SetCursorPosition(center, height + currentLine++);
         Console.Write(line);
     }
 
     currentLine = 0;
+    var restart = false;
     while (typed.Length < randomWords[currentLine].Length)
     {
-        Console.SetCursorPosition(center + typed.Length, height+ currentLine);
+        Console.SetCursorPosition(center + typed.Length, height + currentLine);
         var key = Console.ReadKey(true);
-        if(key is { Key: ConsoleKey.R, Modifiers: ConsoleModifiers.Control })
-            Start();
+        if (key is { Key: ConsoleKey.R, Modifiers: ConsoleModifiers.Control })
+        {
+            restart = true;
+            break;
+        }
+
+
         if (key.Key == ConsoleKey.Backspace)
         {
             if (typed.Length > 0)
             {
                 Console.ResetColor();
-                Console.SetCursorPosition(center + typed.Length - 1,  height + currentLine);
+                Console.SetCursorPosition(center + typed.Length - 1, height + currentLine);
                 Console.Write(randomWords[currentLine][typed.Length - 1]);
                 typed = typed[..^1];
             }
@@ -41,7 +43,9 @@ using ConsoleTypePractice;
             continue;
         }
         else
+        {
             typed += key.KeyChar;
+        }
 
         var i = typed.Length - 1;
         if (i < 0) continue;
@@ -59,11 +63,15 @@ using ConsoleTypePractice;
             }
         }
     }
+
+    if (restart) continue;
+    Console.Clear();
+    Console.ResetColor();
+    Console.SetCursorPosition((Console.WindowWidth - 25) / 2, 4 + Console.CursorTop);
+    Console.WriteLine("enter or space to restart");
+    Console.SetCursorPosition((Console.WindowWidth - 16) / 2, Console.CursorTop);
+    Console.WriteLine("ctrl + c to exit");
+    Console.SetCursorPosition(Console.WindowWidth / 2, Console.CursorTop);
+    if (Console.ReadKey().Key is ConsoleKey.Enter or ConsoleKey.Spacebar) continue;
+    break;
 }
-Start();
-Console.Clear();
-Console.ResetColor();
-Console.SetCursorPosition((Console.WindowWidth - 40) / 2, (Console.WindowHeight / 3) + Console.CursorTop);
-Console.WriteLine("restart: ctrl + r anything else exists");
-if(Console.ReadKey() is { Key: ConsoleKey.R, Modifiers: ConsoleModifiers.Control })
-    Start();
